587e74a642fc5a08df2e98455db15bb94b426ec4
adityafrhan <adityafarhan@students.amikom.ac.id>
2023-03-16 14:55:57 +0700

    melengkapi file calculator

--- Calculator.cs
+++ Calculator.cs
@@ -2,7 +2,15 @@ class calculator
 {
 	static void Main(string[] args)
 	{
-		
+		int a = 10;
+            int b = 6;
+
+            Console.WriteLine("hasil penambahan" + a + " + " + b + " = " + Penambahan(a, b));
+            Console.WriteLine("hasil pengurangan {0} - {1} = {2}", a, b, Pengurangan(a, b));
+            
+
+            Console.WriteLine("\ntekan sembarang key untuk keluar");
+            Console.ReadKey();
 	}
 	
 	static int penambahan(int a, int b)
@@ -13,6 +21,7 @@ class calculator
 	static int pengurangan(int a, int b)
 	{
 		return a - b;
-	}	
+	}
+	
 	
 }
